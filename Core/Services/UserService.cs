45cf81f194c2017680a605519e097ea9ddf26959
sekhubede <msekhubede@gmail.com>
2025-04-04 12:01:41 +0200

    Create UserService.cs

---
+++ Core/Services/UserService.cs
@@ -0,0 +1,30 @@
+﻿using Core.Interfaces;
+using Core.Models;
+using Infrastructure.Interfaces;
+
+namespace Core.Services;
+
+public class UserService : IUserService
+{
+    private readonly List<User> _users = new();
+    private readonly ILoggerService _logger;
+
+    public UserService(ILoggerService logger)
+    {
+        _logger = logger;
+    }
+
+    public void AddUser(string name)
+    {
+        _users.Add(new User { name = name });
+        _logger.LogInformation($"User '{name}' added successfully!");
+        Console.WriteLine("User '{name}' added successfully!");
+    }
+
+    public void ListUsers()
+    {
+        _logger.LogInformation("Listing users...");
+        Console.WriteLine("\nUser List:");
+        _users.ForEach(user => Console.WriteLine($"- {user.name}"));
+    }
+}
