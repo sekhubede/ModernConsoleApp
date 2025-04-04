b3907b540f45831bdffccaded02dae4a28ba650f
sekhubede <msekhubede@gmail.com>
2025-04-04 14:36:47 +0200

    Add tests

---
+++ Tests/LoggerServiceTests.cs
@@ -0,0 +1,31 @@
+﻿using Core.Interfaces;
+using Infrastructure.Services;
+using Moq;
+using Serilog;
+
+namespace Tests;
+
+public class LoggerServiceTests
+{
+    private readonly Mock<ILogger> _serilogMock;
+    private readonly ILoggerService _loggerService;
+
+    public LoggerServiceTests()
+    {
+        _serilogMock = new Mock<ILogger>();
+        _loggerService = new LoggerService(_serilogMock.Object);
+    }
+
+    [Fact]
+    public void LogInformation_ShouldCallSerilogInformation()
+    {
+        // Arrange
+        var message = "This is an info log";
+
+        // Act
+        _loggerService.LogInformation(message);
+
+        // Assert
+        _serilogMock.Verify(logger => logger.Information(It.Is<string>(s => s.Contains(message))), Times.Once);
+    }
+}
---
+++ Tests/UserServiceTests.cs
@@ -0,0 +1,44 @@
+﻿using Core.Interfaces;
+using Infrastructure.Services;
+using Moq;
+
+namespace Tests;
+
+public class UserServiceTests
+{
+    private readonly Mock<ILoggerService> _loggerMock;
+    private readonly IUserService _userService;
+
+    public UserServiceTests()
+    {
+        _loggerMock = new Mock<ILoggerService>();
+        _userService = new UserService(_loggerMock.Object);
+    }
+
+    [Fact]
+    public void AddUser_ShouldAddUserAndLogInformation()
+    {
+        // Arrange
+        var userName = "John Doe";
+
+        // Act
+        _userService.AddUser(userName);
+
+        // Assert
+        _loggerMock.Verify(logger => logger.LogInformation(It.Is<string>(s => s.Contains($"User '{userName}' added successfully"))), Times.Once);
+    }
+
+    [Fact]
+    public void ListUsers_ShouldLogInformaiton()
+    {
+        // Arrange
+        _userService.AddUser("John Doe");
+        _userService.AddUser("Jane Smith");
+
+        // Act
+        _userService.ListUsers();
+
+        // Assert
+        _loggerMock.Verify(logger => logger.LogInformation(It.Is<string>(s => s.Contains("Listing users..."))), Times.Once);
+    }
+}
