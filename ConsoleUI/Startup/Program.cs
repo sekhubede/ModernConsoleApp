a908bcb30b2eaeb3f08565e85ef84db15997b9dd
sekhubede <msekhubede@gmail.com>
2025-04-04 10:58:39 +0200

    Setup consoleui layer

---
+++ ConsoleUI/Startup/Program.cs
@@ -0,0 +1,15 @@
+﻿var services = ConfigureServices();
+var serviceProvider = services.BuildServiceProvider();
+
+static IServiceCollection ConfigureServices()
+{
+    var services = new ServiceCollection();
+
+    // Register Core Services
+    services.AddSingleton<IUserService, UserService>();
+
+    // Register UI Services
+
+
+    // Register Infrastructure Services
+}
