2c66163dd3646b42da2e505b42e8600e3b3417d0
sekhubede <msekhubede@gmail.com>
2025-04-04 10:58:07 +0200

    Setup infrastructure layer

---
+++ Infrastructure/Interfaces/ILoggerService.cs
@@ -0,0 +1,10 @@
+﻿namespace Infrastructure.Interfaces;
+
+public interface ILoggerService
+{
+    void LogInformation(string message);
+    void LogWarning(string message);
+    void LogError(string message);
+    void LogCritical(string message);
+    void LogDebug(string message);
+}
---
+++ Infrastructure/Services/LoggerService.cs
@@ -0,0 +1,22 @@
+﻿using Serilog;
+
+namespace Infrastructure.Services;
+
+public class LoggerService
+{
+    private readonly ILogger _logger;
+
+    public LoggerService()
+    {
+        _logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .WriteTo.Console()
+            .CreateLogger();
+    }
+
+    public void LogInformation(string message) => _logger.Information(message);
+    public void LogWarning(string message) => _logger.Warning(message);
+    public void LogError(string message) => _logger.Error(message);
+    public void LogCritical(string message) => _logger.Fatal(message);
+    public void LogDebug(string message) => _logger.Debug(message);
+}
